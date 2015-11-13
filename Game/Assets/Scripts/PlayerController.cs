1b2f8b467fa293db0c862715f9a3fc75b296edab
TinnedBread <i7666108@PC40575.student.bournemouth.ac.uk>
2015-11-13 23:25:14 +0000

    Added Slower Rotation for Background
    ...

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -4,28 +4,33 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     Transform planet;           //used to store a planet transform.
+    Transform bg;               //background
     Vector3 mousePos;
 
 	// Use this for initialization
 	void Start () 
     {
         planet = GameObject.FindGameObjectWithTag("Planet").transform;
+        bg = GameObject.FindGameObjectWithTag("BG").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        float rot = 5f;         //rot stands for rotate or rotation
+        float rotP = 5f; //rot stands for rotate or rotation
+        float rotBG = 2f;
         mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));      //this gets the mouse position and stores it in a vector called mouse pos. 
 
         if (Input.GetKey(KeyCode.A))
         {
-            planet.Rotate(-Vector3.forward * rot * Time.deltaTime);               
+            planet.Rotate(-Vector3.forward * rotP * Time.deltaTime);
+            bg.Rotate(-Vector3.forward * rotBG * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            planet.Rotate(Vector3.forward * rot * Time.deltaTime);
+            planet.Rotate(Vector3.forward * rotP * Time.deltaTime);
+            bg.Rotate(Vector3.forward * rotBG * Time.deltaTime);
         }
 
         transform.position = Vector3.Lerp(transform.position, mousePos, 0.1f);         //this then user linear interpolation to smooth out the trasnition from ships position to mouse.
