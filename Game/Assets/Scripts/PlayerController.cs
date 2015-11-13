be74400182f7271763a0ad4cda68a4223e6609d2
shpetim123 <i7625539@PC40578.student.bournemouth.ac.uk>
2015-11-13 23:34:30 +0000

    added bulletspawning to player controls and
    added attached an empty gameobject to player called bulletspawner.

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -4,23 +4,32 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     Transform planet;           //used to store a planet transform.
+    public Transform bulletSpawner;      //used to store the bulletSpawn transform, used for spawning the prefab position
+    public Transform bulletPrefab;      
     Vector3 mousePos;
+    float rot;                  //rot stands for rotate or rotation
 
 	// Use this for initialization
 	void Start () 
     {
         planet = GameObject.FindGameObjectWithTag("Planet").transform;
+        rot = 5f;         
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        float rot = 5f;         //rot stands for rotate or rotation
+        Controls();
+
+	}
+
+    void Controls()
+    {
         mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));      //this gets the mouse position and stores it in a vector called mouse pos. 
 
         if (Input.GetKey(KeyCode.A))
         {
-            planet.Rotate(-Vector3.forward * rot * Time.deltaTime);               
+            planet.Rotate(-Vector3.forward * rot * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.D))
@@ -30,5 +39,14 @@ public class PlayerController : MonoBehaviour {
 
         transform.position = Vector3.Lerp(transform.position, mousePos, 0.1f);         //this then user linear interpolation to smooth out the trasnition from ships position to mouse.
        
-	}
+
+    }
+
+    void Fire()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, bulletSpawner.position, Quaternion.identity);
+        }
+    }
 }
