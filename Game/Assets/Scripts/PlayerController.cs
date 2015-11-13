85655c360d10acd32959e7aa056b0a4f8e5efc43
shpetim123 <i7625539@PC40578.student.bournemouth.ac.uk>
2015-11-13 20:53:57 +0000

    rotation code
    by tim

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+    Transform planet;           //used to store a planet transform.
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        planet = GameObject.FindGameObjectWithTag("Planet").transform;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+        float rot = 5f;         //rot stands for rotate or rotation
+        if (Input.GetKey(KeyCode.A))
+        {
+            planet.Rotate(-Vector3.forward * rot * Time.deltaTime);               
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            planet.Rotate(Vector3.forward * rot * Time.deltaTime);
+        }
+	   
 	}
 }
