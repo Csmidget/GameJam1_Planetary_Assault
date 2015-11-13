0daa7fce43b0a4b7434bf78d81d5774b9f5fedcb
Csmidget <csmidge2@hotmail.co.uk>
2015-11-13 20:10:19 +0000

    Folders + 2 scripts
    - Added folders for assets
    - Added PlayerController script, currently empty
    - added Gamestate script and made it singleton

---
+++ Game/Assets/Scripts/Gamestate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gamestate : MonoBehaviour
+{
+
+    [SerializeField]
+
+    public static Gamestate instance = null;
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Singleton GameState already found...");
+        }
+        instance = this;
+    }
+}
