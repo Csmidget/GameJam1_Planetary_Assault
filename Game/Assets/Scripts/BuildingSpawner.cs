38992b8b3f394131c1208700e2c6bf116abf7dbf
TinnedBread <i7666108@PC40575.student.bournemouth.ac.uk>
2015-11-13 23:08:50 +0000

    FIXED CHARLIES SHIT
    fixed the shitty gameobject error

--- Game/Assets/Scripts/BuildingSpawner.cs
+++ Game/Assets/Scripts/BuildingSpawner.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class BuildingSpawner : MonoBehaviour {
 
 	public int numberOfBuildings = 30;
-	public GameObject[] buildings;
+	public Transform[] buildings;
 	public Transform planet;
 	public Transform buildingPrefab;
 	// Use this for initialization
@@ -12,11 +12,12 @@ public class BuildingSpawner : MonoBehaviour {
 	{
 		for (int i = 0; i < numberOfBuildings; i++)
 		{
-			GameObject go = (Instantiate (buildingPrefab,new Vector3 (0, 0, 0),Quaternion.identity) as GameObject);
+			Transform go = (Transform)GameObject.Instantiate(buildingPrefab, new Vector3 (0, 0, 0), Quaternion.identity);
 	//		go.transform.parent = planet.transform;
+            print(go);
 			Transform goTransform;
 			goTransform = go.GetComponent<Transform>();
-			goTransform.parent = planet.transform;
+            goTransform.parent = planet.transform;
 			goTransform.position = planet.position;
 			goTransform.Rotate(new Vector3(1,0,0));
 			goTransform.Translate (new Vector3(0,planet.GetComponent<CircleCollider2D>().radius));
