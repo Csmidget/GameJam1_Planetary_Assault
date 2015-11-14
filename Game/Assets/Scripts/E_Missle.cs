8e63fee962493f13dc2a7989a79739a5adffee10
shpetim123 <i7625539@PC40578.student.bournemouth.ac.uk>
2015-11-14 12:22:52 +0000

    added E_Missle script

---
+++ Game/Assets/Scripts/E_Missle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class E_Missle : MonoBehaviour {
+
+    Rigidbody2D rb;
+    float thrust;
+
+	// Use this for initialization
+	void Start () 
+    {
+        rb = GetComponent<Rigidbody2D>();
+        thrust = 2f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        rb.AddForce(transform.up * thrust);
+	}
+}
