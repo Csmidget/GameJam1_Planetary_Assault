90ebb20b7d5826bd5e3008a188ded75500d7c80a
TinnedBread <i7666108@PC40575.student.bournemouth.ac.uk>
2015-11-14 22:58:01 +0000

    New master

---
+++ Game/Assets/Scripts/Gamestate.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gamestate : MonoBehaviour
+{
+    bool isPaused = false;
+    bool lvl1Cleared = false;
+    bool lvl2Cleared = false;
+    bool lvl3Cleared = false;
+    bool lvl4Cleared = false;
+    bool lvl5Cleared = false;
+
+    [SerializeField]
+
+    public static Gamestate instance = null;
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Singleton GameState already found...");
+        }
+        instance = this;
+        Object.DontDestroyOnLoad(transform.gameObject);
+    }
+
+    void Update()
+    {
+        //Main Menu
+        if (Application.loadedLevel == 0)
+        {
+            if (lvl1Cleared)
+            {
+                //Unlock level 2
+                //lvl
+            }
+            if (lvl2Cleared)
+            {
+                //Unlock level 3
+            }
+            if (lvl3Cleared)
+            {
+                //Unlock level 4
+            }
+            if (lvl4Cleared)
+            {
+                //Unlock level 5
+            }
+            if (lvl5Cleared)
+            {
+                //Player Wins!
+            }
+        }
+    }
+}
