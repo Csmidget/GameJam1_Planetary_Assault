3378c44773ecb2f26df6002a90b3ac0a5e22f1e2
shpetim123 <i7625539@PC40578.student.bournemouth.ac.uk>
2015-11-14 01:13:35 +0000

    added bomb script and building script
    this include building health and bomb damage

---
+++ Game/Assets/Scripts/Bomb.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour {
+    int damage;
+
+	// Use this for initialization
+	void Start () 
+    {
+        damage = 30;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    public int GetDamage()
+    {
+        return damage;
+    }
+}
---
+++ Game/Assets/Scripts/Building.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Building : MonoBehaviour {
+
+    int health;
+    //float money;
+    
+
+	// Use this for initialization
+	void Start () 
+    {
+        health = 100;
+       // money = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+ 
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Bomb" && health >= 0)
+        {
+            int dmgTaken = col.gameObject.GetComponent<Bomb>().GetDamage();
+            health -= dmgTaken;
+            Destroy(col.gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
