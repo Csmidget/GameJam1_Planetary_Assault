9d893b01b7c9585f66a34a976d052a749423216c
Csmidget <csmidge2@hotmail.co.uk>
2015-11-14 17:36:09 +0000

    Fixed turrets shooting from offscreen
    Rewrote code to take in to account distance from main camera

--- Game/Assets/Scripts/AIGunState.cs
+++ Game/Assets/Scripts/AIGunState.cs
@@ -5,6 +5,8 @@ public class AIGunState : MonoBehaviour
 {
     private AIGunAim aiGunAim;
     private AIGunFire aiGunFire;
+    public int shootdistance = 15;
+   Transform cam;
 	// Use this for initialization
 	void Start () 
     {
@@ -12,23 +14,34 @@ public class AIGunState : MonoBehaviour
         aiGunFire = GetComponent<AIGunFire>();
         aiGunAim.enabled = false;
         aiGunFire.enabled = false;
+        cam = GameObject.FindGameObjectWithTag("MainCamera").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-	
+        if (Vector2.Distance(cam.position, transform.position) > shootdistance)
+        {
+            aiGunAim.enabled = false;
+            aiGunFire.enabled = false;
+        }
+        else
+        {
+            aiGunAim.enabled = true;
+            aiGunFire.enabled = true;
+        }
+
 	}
 
-    void OnBecameVisible()
-    {
-        aiGunAim.enabled = true;
-        aiGunFire.enabled = true;
-    }
+  //  void OnBecameVisible()
+  //  {
+  //      aiGunAim.enabled = true;
+  //      aiGunFire.enabled = true;
+  //  }
 
-    void OnBecameInvisible()
-    {
-        aiGunAim.enabled = false;
-        aiGunFire.enabled = false;
-    }
+ //   void OnBecameInvisible()
+  //  {
+ //       aiGunAim.enabled = false;
+ //       aiGunFire.enabled = false;
+ //   }
 }
