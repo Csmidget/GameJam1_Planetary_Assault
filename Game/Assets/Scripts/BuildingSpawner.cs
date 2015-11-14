2a7ddad88ebfaa389e5f206e8df9490d16916081
Csmidget <csmidge2@hotmail.co.uk>
2015-11-14 00:34:38 +0000

    added building2 to building generator

--- Game/Assets/Scripts/BuildingSpawner.cs
+++ Game/Assets/Scripts/BuildingSpawner.cs
@@ -1,26 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System
 
 public class BuildingSpawner : MonoBehaviour {
 
 	public int numberOfBuildings = 30;
 	public Transform[] buildings;
 	public Transform planet;
-	public Transform buildingPrefab;
+	public Transform buildingPrefab1;
+    public Transform buildingPrefab2;
 	// Use this for initialization
 	void Start ()
 	{
 		for (int i = 0; i < numberOfBuildings; i++)
 		{
-			Transform go = (Transform)GameObject.Instantiate(buildingPrefab, new Vector3 (0, 0, 0), Quaternion.identity);
+            
+            int randnum = Random.Range(0,2);
+            print(randnum);
+
+            //FIX THIS
+            if (randnum == 0)
+            {
+                Transform go = (Transform)GameObject.Instantiate(buildingPrefab1, new Vector3(0, 0, 0), Quaternion.identity);
+                Transform goTransform;
+                goTransform = go.GetComponent<Transform>();
+                goTransform.parent = planet.transform;
+                goTransform.position = planet.position;
+                goTransform.Rotate(new Vector3(0, 0, Random.Range(0, 360)));
+                goTransform.Translate(new Vector3(0, planet.GetComponent<CircleCollider2D>().radius * planet.localScale.x + 0.3f));
+                buildings[i] = go;
+            }
+            
+            else
+            {
+                Transform go = (Transform)GameObject.Instantiate(buildingPrefab2, new Vector3(0, 0, 0), Quaternion.identity);
+                Transform goTransform;
+                goTransform = go.GetComponent<Transform>();
+                goTransform.parent = planet.transform;
+                goTransform.position = planet.position;
+                goTransform.Rotate(new Vector3(0, 0, Random.Range(0, 360)));
+                goTransform.Translate(new Vector3(0, planet.GetComponent<CircleCollider2D>().radius * planet.localScale.x + 0.3f));
+                buildings[i] = go;
+            };
+
 	//		go.transform.parent = planet.transform;
-			Transform goTransform;
-			goTransform = go.GetComponent<Transform>();
-            goTransform.parent = planet.transform;
-			goTransform.position = planet.position;
-			goTransform.Rotate(new Vector3(0,0,Random.Range(0,360)));
-			goTransform.Translate (new Vector3(0,planet.GetComponent<CircleCollider2D>().radius * planet.localScale.x + 0.5f));
-			buildings[i] = go;
+		//	Transform goTransform;
+		//	goTransform = go.GetComponent<Transform>();
+       //     goTransform.parent = planet.transform;
+		//	goTransform.position = planet.position;
+	//		goTransform.Rotate(new Vector3(0,0,Random.Range(0,360)));
+	//		goTransform.Translate (new Vector3(0,planet.GetComponent<CircleCollider2D>().radius * planet.localScale.x + 0.3f));
+	//		buildings[i] = go;
 		}
 	}
 
