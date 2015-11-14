4bb35c93b95c583d3984a47652a1523acdcdfd8f
TinnedBread <i7666108@PC40575.student.bournemouth.ac.uk>
2015-11-14 14:33:02 +0000

    Added Bomb Collision to the Planet
    If a bomb now hits the planet, the bomb will be destroyed. (See
    PlanetCollision Script)

---
+++ Game/Assets/Scripts/PlanetCollision.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetCollision : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Bomb")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
