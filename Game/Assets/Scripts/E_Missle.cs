b304c92df5ae991bdc9a6077ddea3bdc642cade9
shpetim123 <i7625539@PC40578.student.bournemouth.ac.uk>
2015-11-14 13:13:16 +0000

    E_missle should have slerp
    hasnt been tested yet

--- Game/Assets/Scripts/E_Missle.cs
+++ Game/Assets/Scripts/E_Missle.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class E_Missle : MonoBehaviour {
-
+    public Transform playerShip;        //stores the players position.
+    Vector3 storeTransform;     //stores the trasnform once to a vector3, this is so the missle doesnt home in.
     Rigidbody2D rb;
     float thrust;
 
@@ -10,12 +11,14 @@ public class E_Missle : MonoBehaviour {
 	void Start () 
     {
         rb = GetComponent<Rigidbody2D>();
+        storeTransform = playerShip.position;
+        
         thrust = 2f;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () 
     {
-        rb.AddForce(transform.up * thrust);
+        rb.AddForce(Vector3.Slerp(transform.up * thrust, storeTransform, 0.5f));
 	}
 }
