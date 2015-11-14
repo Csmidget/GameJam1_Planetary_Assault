7687654258f21a5b1cd3521bcdb57055702ec23e
Csmidget <csmidge2@hotmail.co.uk>
2015-11-14 20:13:07 +0000

    started on bomb ui

---
+++ Game/Assets/Scripts/UI/BombUIScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombUIScript : MonoBehaviour {
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	player = GameObject.FindGameObjectWithTag("Player")
+    player.GetComponent<PlayerGunFire>().
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
