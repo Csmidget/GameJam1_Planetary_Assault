c5297c023435b69390f61805dda22574e770df48
TinnedBread <i7666108@PC40575.student.bournemouth.ac.uk>
2015-11-14 22:12:15 +0000

    Change Score

--- Game/Assets/Scripts/Gamestate.cs
+++ Game/Assets/Scripts/Gamestate.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class Gamestate : MonoBehaviour
 {
     bool isPaused = false;
+    bool lvl1Cleared = false;
+    bool lvl2Cleared = false;
+    bool lvl3Cleared = false;
+    bool lvl4Cleared = false;
+    bool lvl5Cleared = false;
+
     [SerializeField]
 
     public static Gamestate instance = null;
@@ -16,5 +22,31 @@ public class Gamestate : MonoBehaviour
         instance = this;
     }
 
-   
+    void Update()
+    {
+        //Main Menu
+        if (Application.loadedLevel == 0)
+        {
+            if (lvl1Cleared)
+            {
+                //Unlock level 2
+            }
+            if (lvl2Cleared)
+            {
+                //Unlock level 3
+            }
+            if (lvl3Cleared)
+            {
+                //Unlock level 4
+            }
+            if (lvl4Cleared)
+            {
+                //Unlock level 5
+            }
+            if (lvl5Cleared)
+            {
+                //Player Wins!
+            }
+        }
+    }
 }
